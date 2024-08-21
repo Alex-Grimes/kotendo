a4adef5f7d09c46594d9a87adc759a530179c60d
alex <alexgrimes.se@gmail.com>
2024-08-21 12:25:20 -0400

    Working through CPU

--- src/main/kotlin/org/kotendo/cpu/Bus.kt
+++ src/main/kotlin/org/kotendo/cpu/Bus.kt
@@ -1,13 +1,52 @@
 package org.kotendo.cpu
 
 import org.kotendo.Address
+import org.kotendo.Apu
+import org.kotendo.KeyPad
+import org.kotendo.ppu.Ppu
+import org.kotendo.rom.PrgRom
+import org.kotendo.toUint
+import java.lang.IllegalStateException
 
-class  Bus {
+class  Bus(private val cpuRam: Ram, private val ppu: Ppu, private val prgRom: PrgRom, private val keyPad: KeyPad){
+    private val apu = Apu()
+
     fun read(addr: Address): Int {
-        return 1
+        return when (addr.value) {
+            in 0x0000.until(0x0800) -> cpuRam.read(addr)
+            in 0x0800.until(0x2000) -> cpuRam.read(addr.minus(0x0800))
+            in 0x2000.until(0x4000) -> ppu.read(addr.minus(0x2000))
+            0x4016 -> keyPad.read()
+            0x4017 -> 0
+            in 0x4000.until(0x4020) -> apu.read(addr.minus(0x4000))
+            in 0x4020.until(0x6000) -> throw IllegalStateException("Extended ROM isn't supported")
+            in 0x6000.until(0x8000) -> { /* Need to take care of extended RAM access? */ 0
+            }
+
+            in 0x8000.until(0xC000) -> prgRom.read(addr.minus(0x8000))
+            else -> prgRom.read(addr.minus(if (prgRom.size <= 0x4000) 0xC000 else 0x8000))
+        }
+    }
+
+    fun readWord(addr: Address): Int {
+        val lsb = read(addr) and 0xFF
+        val msb = read(addr.plus(1)) and 0xFF
+        return msb shl 8 or lsb
     }
 
     fun write(addr: Address, value: Byte) {
-        return
+        when (addr.value) {
+            in 0x0000.until(0x0800) -> cpuRam.write(addr, value)
+            in 0x0800.until(0x2000) -> cpuRam.write(addr.minus(0x0800), value)
+            in 0x2000.until(0x4000) -> ppu.write(addr.minus(0x2000), value)
+            0x4014 -> {
+                val srcBytes = cpuRam.slice(addr = Address(value * 0x100), size = 0x100)
+                ppu.transferToSprite(srcBytes)
+            }
+            0x4016 -> keyPad.write(value = value.toUint())
+            0x4017 -> {}
+            in 0x4000.until(0x4020) -> { /* TODO */ }
+            else -> {}
+        }
     }
 }
--- src/main/kotlin/org/kotendo/cpu/Cpu.kt
+++ src/main/kotlin/org/kotendo/cpu/Cpu.kt
@@ -1,4 +1,62 @@
 package org.kotendo.cpu
 
-class Cpu {
+import org.kotendo.Address
+import org.kotendo.Interrupt
+import org.kotendo.toUint
+import java.lang.RuntimeException
+
+class Cpu(
+    private val cpuBus: Bus,
+    private val interrupt: Interrupt
+) {
+    internal val register = Register()
+    internal val stack = Stack(bus = cpuBus, register = register)
+
+    fun reset() {
+        register.pc = cpuBus.readWord(Address(0xFFFC))
+        register.sp = 0xFF
+        register.statusInterruptDisabled = true
+        register.statusDecimalMode = false
+    }
+
+    private fun processNmi() {
+        register.statusBreakMode = false
+        val pc = register.pc
+        stack.push((pc shr 8).toByte())
+        stack.push((pc and 0xFF).toByte())
+        stack.push(register.sp.toByte())
+        register.statusInterruptDisabled = true
+        val addr = cpuBus.readWord(Address(0xFFFA))
+        register.pc = addr
+    }
+
+    fun fetch(): Int {
+        val byte = cpuBus.read(register.pcAsAddress())
+        register.pc++
+        return byte
+    }
+
+    fun fetchWord(): Int {
+        val word = cpuBus.readWord(register.pcAsAddress())
+        register.pc++
+        register.pc++
+        return word
+    }
+
+    fun fetchAsAddress() = Address(fetch())
+
+    fun fetchWordAsAddress() = Address(fetchWord())
+
+    private fun fetchOperand(addressingMode: AddressingMode): Operand {
+        return when (addressingMode) {
+            AddressingMode.IMPLICIT -> Operand()
+            AddressingMode.ACCUMULATOR -> Operand(
+                valueFunction = { register.a }
+            )
+            AddressingMode.IMMEDIATE -> fetch().let { value -> Operand(raw = value, valueFunction = {value}) }
+
+        }
+
+    }
+
 }
